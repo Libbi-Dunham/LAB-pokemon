a055e400964074ea0577d7ca70f003962fd88383
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-21 11:58:22 -0700

    fixing generator

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import { pokemonArr } from './data/pokemon.js';
 console.log(pokemonArr);
+import { encounterPokemon, capturePokemon } from './storage-utils.js';
 
-const caughtSpan = document.getElementById('caught');
-const chosenSpan = document.getElementById('chosen');
-const appearSpan = document.getElementById('appeared');
+
 
 const pokemonImg1 = document.getElementById('pokemon-img1');
 const pokemonImg2 = document.getElementById('pokemon-img2');
 const pokemonImg3 = document.getElementById('pokemon-img3');
-const button = document.getElementById('choose');
+const pokemonRadio1 = document.getElementById('pokemon-radio1');
+const pokemonRadio2 = document.getElementById('pokemon-radio2');
+const pokemonRadio3 = document.getElementById('pokemon-radio3');
 
-let caught = 0;
-let chosen = 0;
-let appeared = 0;
+const button = document.getElementById('choose');
 
 
 const generatePokemon = ()=>{
@@ -33,23 +32,40 @@ const generatePokemon = ()=>{
     console.log(random1, random2);
 
     let pokemon1 = pokemonArr[random1];
+    capturePokemon(pokemon1.id);
     pokemonImg1.src = pokemon1.url_image;
+    pokemonRadio1.value = pokemon1.id;
 
     let pokemon2 = pokemonArr[random2];
+    capturePokemon(pokemon2.id);
     pokemonImg2.src = pokemon2.url_image;
+    pokemonRadio2.value = pokemon2.id;
 
     let pokemon3 = pokemonArr[random3];
+    capturePokemon(pokemon3.id);
     pokemonImg3.src = pokemon3.url_image;
+    pokemonRadio3.value = pokemon3.id;
 };
-
+let totalPlays = 0;
 generatePokemon();
 
 button.addEventListener('click', ()=>{
-    const selected = document.querySelector('input[type=radio]:checked');
+    totalPlays++;
+    const chosenRadio = document.querySelector('input[type=radio]:checked');
+    const chosenId = chosenRadio.value;
+    console.log(chosenId);
+    encounterPokemon(chosenId);
 
     generatePokemon();
-    appeared++;
-    appeared++;
-    appeared++;
-    appearSpan.textContent = appeared;
+
+    if (chosenRadio){
+        const chosenId = chosenRadio.value;
+        totalPlays++;
+        encounterPokemon(chosenId);
+        if (totalPlays >= 3){
+            window.location = './results';
+        } else {
+            generatePokemon();
+        }
+    }
 });
