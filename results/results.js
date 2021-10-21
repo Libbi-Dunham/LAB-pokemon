69973494949ed0b95220fbe46cfbf8c3dffaff58
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-21 16:15:17 -0700

    working on results page

---
+++ results/results.js
@@ -0,0 +1,26 @@
+import { pokemonArr } from '../data/pokemon.js';
+import { getResults, findById } from '../data/storageUtils';
+
+const results = getResults();
+
+const main = document.getElementById('main');
+
+for (let item of results){
+    const pokemon = findById(item.id, pokemon);
+    console.log(item);
+    console.log(pokemonArr);
+
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = pokemon.url_image;
+    const header = document.createElement('h2');
+    const resultSpan1 = document.createElement('span');
+    resultSpan1.textContent = `encounter: ${item.encounter}`;
+    const resultSpan2 = document.createElement('span');
+    resultSpan2.textContent = `encounter: ${item.encounter}`;
+
+    div.append(header, img, resultSpan1, resultSpan2);
+    main.append(div);
+
+
+}
