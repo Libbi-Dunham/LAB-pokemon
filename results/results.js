e22d0250751d2706ac37e17c1b847da0fb175476
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-22 14:21:03 -0700

    added encounter to my chart and fixed my styles

--- results/results.js
+++ results/results.js
@@ -32,6 +32,8 @@ const names = results.map((item)=>{
 });
 
 const capture = results.map(item=>item.capture);
+const encounter = results.map(item=> item.encounter);
+// console.log(encounter);
 
 var ctx = document.getElementById('myChart').getContext('2d');
 // eslint-disable-next-line no-undef
@@ -40,6 +42,7 @@ new Chart(ctx, {
     data: {
         labels: names,
         datasets: [{
+            
             label: 'number of times captured',
             data: capture,
             backgroundColor: [
@@ -59,7 +62,29 @@ new Chart(ctx, {
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
-        }]
+
+            
+        }, 
+        {
+            label: 'number of times encounter',
+            data: encounter,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1 }]
     },
     options: {
         scales: {
